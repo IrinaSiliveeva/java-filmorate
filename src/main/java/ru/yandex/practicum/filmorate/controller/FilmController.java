6eda57adc7f7691c922f84366787e74d60763029
Siliveeva <siliveeva@yandex.ru>
2022-07-01 20:27:59 +0300

    controllers-films-users

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/films")
+public class FilmController {
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,26 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    private int id;
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String login;
+    private String name;
+    @Past
+    private LocalDate birthday;
+}
